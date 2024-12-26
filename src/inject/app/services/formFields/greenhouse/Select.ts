dbf25a07738a32a6b63f3a5cd68b611107472f3f
berellevy <berellevy@gmail.com>
2024-12-26 09:03:46 -0500

    string answer dto and formatting

--- src/inject/app/services/formFields/greenhouse/Select.ts
+++ src/inject/app/services/formFields/greenhouse/Select.ts
@@ -1,43 +1,43 @@
-import fieldFillerQueue from '@src/shared/utils/fieldFillerQueue'
-import { getElement } from '@src/shared/utils/getElements'
-import { GreenhouseBaseInput } from './GreenhouseBaseInput'
-import { xpaths } from './xpaths'
-import AnswerDTO from '../../DTOs/AnswerDTO'
+import fieldFillerQueue from '@src/shared/utils/fieldFillerQueue';
+import { getElement } from '@src/shared/utils/getElements';
+import { GreenhouseBaseInput } from './GreenhouseBaseInput';
+import { xpaths } from './xpaths';
+import StringAnswerDTO from '../../DTOs/StringAnswerDTO';
 
 export class Select extends GreenhouseBaseInput {
-  static XPATH: string = xpaths.BASIC_SELECT
-  fieldType = 'Dropdown'
+  static XPATH: string = xpaths.BASIC_SELECT;
+  fieldType = 'Dropdown';
 
   get selectElement(): HTMLSelectElement {
-    return getElement(this.element, './/select') as HTMLSelectElement
+    return getElement(this.element, './/select') as HTMLSelectElement;
   }
 
   inputElement(): HTMLInputElement {
-    return this.selectElement as unknown as HTMLInputElement
+    return this.selectElement as unknown as HTMLInputElement;
   }
   listenForChanges(): void {
-    this.selectElement.addEventListener("change", () => {
-      this.triggerReactUpdate()
-    })
+    this.selectElement.addEventListener('change', () => {
+      this.triggerReactUpdate();
+    });
   }
   get selectedAnswerElement(): HTMLOptionElement {
-    return this.selectElement.selectedOptions[0]
+    return this.selectElement.selectedOptions[0];
   }
   currentValue(): string {
-    return this.selectElement.selectedOptions[0]?.innerText || ''
+    return this.selectElement.selectedOptions[0]?.innerText || '';
   }
-  async fill(answers: AnswerDTO<string>[]): Promise<void> {
+  async fill(answers: StringAnswerDTO[]): Promise<void> {
     await fieldFillerQueue.enqueue(async () => {
       for (const answer of answers) {
         const correctAnswerElement = getElement(
           this.selectElement,
           `./option[text() = "${answer.answer}"]`
-        ) as HTMLOptionElement
+        ) as HTMLOptionElement;
         if (correctAnswerElement) {
-          this.selectElement.selectedIndex = correctAnswerElement.index
-          break
+          this.selectElement.selectedIndex = correctAnswerElement.index;
+          break;
         }
       }
-    })
+    });
   }
 }
