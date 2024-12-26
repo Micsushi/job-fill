698b144d9b9f654baaa936ec5881c99308913e5d
berellevy <berellevy@gmail.com>
2024-12-26 09:05:43 -0500

    formatting and string answer dto

--- src/inject/app/services/formFields/greenhouseReact/CheckboxMulti.ts
+++ src/inject/app/services/formFields/greenhouseReact/CheckboxMulti.ts
@@ -1,62 +1,58 @@
-import fieldFillerQueue from "@src/shared/utils/fieldFillerQueue";
-import { getElement, getElements } from "@src/shared/utils/getElements";
-
-import { GreenhouseReactBaseInput } from "./GreenhouseReactBaseInput";
-import { xpaths } from "./xpaths";
-import { CheckboxWrapperContainer } from "./ElementWrappers/CheckboxWrapperContainer";
-import AnswerDTO from "../../DTOs/AnswerDTO";
+import fieldFillerQueue from '@src/shared/utils/fieldFillerQueue';
+import { getElement, getElements } from '@src/shared/utils/getElements';
+import { GreenhouseReactBaseInput } from './GreenhouseReactBaseInput';
+import { xpaths } from './xpaths';
+import { CheckboxWrapperContainer } from './ElementWrappers/CheckboxWrapperContainer';
+import StringAnswerDTO from '../../DTOs/StringAnswerDTO';
 
 export class CheckboxMulti extends GreenhouseReactBaseInput {
-  static XPATH = xpaths.CHECKBOX_MULTI
-  fieldType = "Dropdown"
+  static XPATH = xpaths.CHECKBOX_MULTI;
+  fieldType = 'Dropdown';
 
   get labelElement(): HTMLElement {
-    return getElement(
-      this.element,
-      `.//legend[starts-with(@class, "label")]`
-    )
+    return getElement(this.element, `.//legend[starts-with(@class, "label")]`);
   }
 
   listenForChanges(): void {
-    this.element.addEventListener("click", (e) => {
-      const { tagName } = (e.target as HTMLElement)
-      if (tagName === "INPUT") {
-        this.triggerReactUpdate()
+    this.element.addEventListener('click', (e) => {
+      const { tagName } = e.target as HTMLElement;
+      if (tagName === 'INPUT') {
+        this.triggerReactUpdate();
       }
-    })
+    });
   }
 
   /**
    * for now treat multis like singles.
    */
   currentValue() {
-    const firstSelectedElement = this.choiceElements.find(el => el.checked)
-    return firstSelectedElement?.value || ""
+    const firstSelectedElement = this.choiceElements.find((el) => el.checked);
+    return firstSelectedElement?.value || '';
   }
 
   get choiceElements(): CheckboxWrapperContainer[] {
-    return getElements(
-      this.element,
-      `.//div[@class="checkbox__wrapper"]`
-    ).map((el) => new CheckboxWrapperContainer(el))
+    return getElements(this.element, `.//div[@class="checkbox__wrapper"]`).map(
+      (el) => new CheckboxWrapperContainer(el)
+    );
   }
 
   public isFilled(current: any, stored: any[]): boolean {
-    return stored.includes(current)
+    return stored.includes(current);
   }
 
-  async fill(answers: AnswerDTO<string>[]): Promise<void> {
+  async fill(answers: StringAnswerDTO[]): Promise<void> {
     await fieldFillerQueue.enqueue(async () => {
-      const choiceElements = this.choiceElements
-      choiceElements.forEach(el => el.uncheck())
+      const choiceElements = this.choiceElements;
+      choiceElements.forEach((el) => el.uncheck());
       for (const storedAnswer of answers) {
-        const correctChoice = this.choiceElements.find(el => el.value === storedAnswer.answer)
+        const correctChoice = this.choiceElements.find(
+          (el) => el.value === storedAnswer.answer
+        );
         if (correctChoice) {
-          correctChoice.check()
-          break
+          correctChoice.check();
+          break;
         }
       }
-    })
+    });
   }
 }
-
