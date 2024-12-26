e70af3eaef7990220dd384c54a0d36c026668c9d
berellevy <berellevy@gmail.com>
2024-12-26 09:06:06 -0500

    formatting and file answer dto

--- src/inject/app/services/formFields/greenhouseReact/File.ts
+++ src/inject/app/services/formFields/greenhouseReact/File.ts
@@ -1,89 +1,81 @@
-import { AnswerValueSingleFileUpload } from "../../../MoreInfoPopup/AnswerDisplay/AnswerValueDisplay/AnswerValueSingleFileUpload";
-import { sleep } from "@src/shared/utils/async";
-import fieldFillerQueue from "@src/shared/utils/fieldFillerQueue";
-import { LocalStorageFile, localStorageToFile } from "@src/shared/utils/file";
-import { getElement } from "@src/shared/utils/getElements";
-import { AnswerValueMethods, } from "../baseFormInput";
-import { getReactProps } from "../utils";
-import { GreenhouseReactBaseInput } from "./GreenhouseReactBaseInput";
-import { xpaths } from "./xpaths";
-import { saveButtonClickHandlers } from "../../../hooks/saveButtonClickHandlers";
-import AnswerDTO from "../../DTOs/AnswerDTO";
-
+import { AnswerValueSingleFileUpload } from '../../../MoreInfoPopup/AnswerDisplay/AnswerValueDisplay/AnswerValueSingleFileUpload';
+import { sleep } from '@src/shared/utils/async';
+import fieldFillerQueue from '@src/shared/utils/fieldFillerQueue';
+import { localStorageToFile } from '@src/shared/utils/file';
+import { getElement } from '@src/shared/utils/getElements';
+import { AnswerValueMethods } from '../baseFormInput';
+import { getReactProps } from '../utils';
+import { GreenhouseReactBaseInput } from './GreenhouseReactBaseInput';
+import { xpaths } from './xpaths';
+import { saveButtonClickHandlers } from '../../../hooks/saveButtonClickHandlers';
+import { SingleFileAnswerDTO } from '../../DTOs/SingleFileAnswerDTO';
 
 export class File extends GreenhouseReactBaseInput {
-  static XPATH = xpaths.FILE
-  fieldType = 'SingleFileUpload'
-  public saveButtonClickHandler = saveButtonClickHandlers.withNotice
-  fieldNotice =
-    `##### To save and autofill files, upload them in the 'Answers' section below.
-    \n\n[See how](https://www.youtube.com/watch?v=JYMATq9siIY&t=134s)`
+  static XPATH = xpaths.FILE;
+  fieldType = 'SingleFileUpload';
+  public saveButtonClickHandler = saveButtonClickHandlers.withNotice;
+  fieldNotice = `##### To save and autofill files, upload them in the 'Answers' section below.
+    \n\n[See how](https://www.youtube.com/watch?v=JYMATq9siIY&t=134s)`;
 
   get answerValue() {
     return {
       ...super.answerValue,
       displayComponent: AnswerValueSingleFileUpload,
-    } as AnswerValueMethods
+    } as AnswerValueMethods;
   }
 
   get labelElement(): HTMLElement {
-    return getElement(this.element,
-      `.//div[contains(@class, "label")]`
-    )
+    return getElement(this.element, `.//div[contains(@class, "label")]`);
   }
 
   listenForChanges(): void {
     const observer = new MutationObserver((mutations: MutationRecord[]) => {
-      const XPATH = `self::*[starts-with(@class, "file-upload__filename")]`
+      const XPATH = `self::*[starts-with(@class, "file-upload__filename")]`;
       if (getElement(mutations, XPATH)) {
-        this.triggerReactUpdate()
+        this.triggerReactUpdate();
       }
-    })
+    });
     observer.observe(this.element, {
       childList: true,
       subtree: true,
-    })
+    });
   }
 
   currentValue() {
-    return getElement(
-      this.element,
-      `.//div[@class="file-upload__filename"]`
-    )?.innerText || ""
-
+    return (
+      getElement(this.element, `.//div[@class="file-upload__filename"]`)
+        ?.innerText || ''
+    );
   }
 
   get deleteButtonElement(): HTMLElement {
-    return getElement(
-      this.element,
-      ".//button[@aria-label='Remove file']"
-    )
+    return getElement(this.element, ".//button[@aria-label='Remove file']");
   }
 
   get inputElement(): HTMLInputElement {
     return getElement(
       this.element,
       `.//input[@type="file"]`
-    ) as HTMLInputElement
+    ) as HTMLInputElement;
   }
 
   public isFilled(current: any, stored: any[]): boolean {
-    return current === stored[0].name
+    return current === stored[0].name;
   }
 
   async deleteCurrentFile(): Promise<void> {
     if (this.deleteButtonElement) {
-      this.deleteButtonElement.click()
-      await sleep(500)
+      this.deleteButtonElement.click();
+      await sleep(500);
     }
   }
 
-  async fill(answers: AnswerDTO<LocalStorageFile>[]): Promise<void> {
+  async fill(answers: SingleFileAnswerDTO[]): Promise<void> {
     await fieldFillerQueue.enqueue(async () => {
-      await this.deleteCurrentFile()
-      const file = localStorageToFile(answers[0].answer)
-      const reactProps = getReactProps(this.inputElement)
-      reactProps?.onChange({ target: { files: [file] } })
-    })
+      await this.deleteCurrentFile();
+      const file = localStorageToFile(answers[0].answer);
+      const reactProps = getReactProps(this.inputElement);
+      reactProps?.onChange({ target: { files: [file] } });
+    });
   }
 }
